34b28b822c17379dfe5f63ddf22d3cd6ce7ec569
Dave Howson <dave.sohan@gmail.com>
2020-10-25 16:13:00 +0530

    Code formating fixes

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -4,7 +4,7 @@ import logger from './utilities/logger.util';
 const PORT = 5000;
 const app = express();
 
-app.get('/', (req, res) => res.send('Welcome to i18n-organizer'));
+app.get('/', (req, res) => res.send('Welcome to the react-node-monorepo'));
 
 app.listen(PORT, () => {
   logger.info(`Server is running at http://localhost:${PORT}`);
